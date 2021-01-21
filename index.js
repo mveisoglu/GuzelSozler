3835a9927bcff785c78ef12db285f01945acfff0
mveisoglu <>
2021-01-21 23:27:42 +0300

    v0.2

--- index.js
+++ index.js
@@ -84,6 +84,10 @@ app.route("/api/guzelsozler")
           res.send(err);
       });
     });
-app.listen(5000, function(){
-  console.log("5000 Portuna Bağlandık.")
-})
+    let port = process.env.PORT;
+    if(port == "" || port == null){
+      port = 5000;
+    }
+    app.listen(port, function(){
+      console.log("port numarasi : " + port);
+    });
