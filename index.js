06dc7dd50e368f6bc4acd1c9a0c24033f6ac4262
mveisoglu <>
2021-01-23 21:29:46 +0300

    v.0.6

--- index.js
+++ index.js
@@ -14,99 +14,91 @@ mongoose.connect("mongodb+srv://mveisoglu:mustafa2012@cluster0.gddzt.mongodb.net
   useUnifiedTopology: true
 });
 
-app.get("/", function(req, res) {
-  GuzelSoz.find({}, function(err, gelenSozler) {
-    res.render("anasayfa", {
-      sozler: gelenSozler
-    });
-  });
-});
-
 const guzelSozSema = {
-  kategori: String,
-  icerik: String
+  kategori : String,
+  icerik : String
 };
 const GuzelSoz = mongoose.model("GuzelSoz", guzelSozSema);
+app.get("/", function(req, res){
+  GuzelSoz.find({}, function(err, gelenSozler){
+    res.render("anasayfa", {sozler : gelenSozler});
+  });
+});
 app.route("/api/guzelsoz/:id")
-  .get(function(req, res) {
-    GuzelSoz.findOne({
-      _id: req.params.id
-    }, function(err, gelenVeri) {
-      res.send(gelenVeri);
-    });
-  })
-  .put(function(req, res) {
-    var kategoriGelen = req.body.kategori;
-    var icerikGelen = req.body.icerik;
-    GuzelSoz.update({
-      _id: req.params.id
-    }, {
-      kategori: kategoriGelen,
-      icerik: icerikGelen
-    }, {
-      overwrite: true
-    }, function(err) {
-      if (!err)
-        res.send({ sonuc : "Kayıt başarıyla güncellendi."});
-      else
-        res.send(err);
-    });
-  })
-  .patch(function(req, res) {
-    GuzelSoz.update({
-      _id: req.params.id
-    }, {
-      $set: req.body
-    }, function(err) {
-      if (!err)
-        res.send({ sonuc : "Kayıt başarıyla güncellendi."});
-      else
-        res.send(err);
-    })
-  })
-  .delete(function(req, res) {
-    GuzelSoz.deleteOne({
-      _id: req.params.id
-    }, function(err) {
-      if (!err)
-        res.send({ sonuc : "Kayıt başarıyla silindi."});
-      else
-        res.send(err);
-    })
-  });
-app.route("/api/guzelsozler")
-  .get(function(req, res) {
-    GuzelSoz.find({}, function(err, gelenVeri) {
-      if (!err)
+    .get(function(req, res){
+      GuzelSoz.findOne({_id : req.params.id} , function(err, gelenVeri){
         res.send(gelenVeri);
-      else
-        res.send(err);
+      });
+    })
+    .put(function(req, res){
+      var kategoriGelen = req.body.kategori;
+      var icerikGelen   = req.body.icerik;
+      GuzelSoz.update({_id : req.params.id} , {kategori : kategoriGelen, icerik : icerikGelen}, {overwrite: true}, function(err){
+        if(!err)
+          res.send({sonuc : "Kayıt başarıyla güncellendi."});
+        else
+          res.send(err);
+      });
+    })
+    .patch(function(req, res){
+      GuzelSoz.update({_id : req.params.id} , {$set : req.body}, function(err){
+        if(!err)
+          res.send({sonuc : "Kayıt başarıyla güncellendi."});
+        else
+          res.send(err);
+      })
+    })
+    .delete(function(req, res){
+      var sifre = req.body.sifre;
+      if(sifre == "parola1234"){
+        GuzelSoz.deleteOne({_id : req.params.id}, function(err){
+          if(!err)
+            res.send({sonuc : "Kayıt başarıyla silindi."});
+          else
+            res.send(err);
+        })
+      }else{
+        res.send({sonuc : "Şifre hatalı."});
+      }
     });
-  })
-  .post(function(req, res) {
-    var guzelSoz = new GuzelSoz({
-      kategori: req.body.kategori,
-      icerik: req.body.icerik
+app.route("/api/guzelsozler")
+    .get(function(req, res){
+      GuzelSoz.find({}, function(err, gelenVeri){
+        if(!err)
+          res.send(gelenVeri);
+        else
+           res.send(err);
+      });
+    })
+    .post(function(req, res){
+       var guzelSoz = new GuzelSoz({
+         kategori : req.body.kategori,
+         icerik : req.body.icerik
+       });
+       guzelSoz.save(function(err){
+          if(!err)
+            res.send( {sonuc : "Kayıt başarıyla oluşturuldu."} );
+          else
+            res.send(err);
+       });
+    })
+    .delete(function(req, res){
+      var sifre = req.body.sifre;
+      if(sifre == "parola1234"){
+        GuzelSoz.deleteMany({}, function(err){
+          if(!err)
+            res.send( {sonuc : "Tüm kayıtlar başarıyla silindi."} );
+          else
+            res.send(err);
+        });
+      }else{
+        res.send({sonuc : "Şifre hatalı."});
+      }
     });
-    guzelSoz.save(function(err) {
-      if (!err)
-        res.send(  { sonuc : "Kayıt başarıyla oluşturuldu." }   );
-      else
-        res.send(err);
-    });
-  })
-  .delete(function(req, res) {
-    GuzelSoz.deleteMany({}, function(err) {
-      if (!err)
-        res.send({ sonuc : "Tüm kayıtlar silindi." });
-      else
-        res.send(err);
-    });
-  });
 let port = process.env.PORT;
-if (port == "" || port == null) {
+if(port == "" || port == null){
   port = 5000;
 }
-app.listen(port, function() {
+app.listen(port, function(){
   console.log("port numarasi : " + port);
 });
